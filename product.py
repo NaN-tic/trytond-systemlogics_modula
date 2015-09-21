2a2c0597564e4f851c29320db698679b834bcd38
resteve <resteve@zikzakmedia.com>
2015-09-21 14:22:33 +0200

    IMP ARTICOLI when create/write product/codes

--- product.py
+++ product.py
@@ -3,22 +3,73 @@
 #the full copyright notices and license terms.
 from trytond.pool import Pool, PoolMeta
 
-__all__ = ['ProductCode']
+__all__ = ['Product', 'ProductCode']
 __metaclass__ = PoolMeta
-_TYPE_BARCODES = ['EAN13']
+
+
+class Product:
+    __name__ = 'product.product'
+
+    @classmethod
+    def create(cls, vlist):
+        SystemLogicsModula = Pool().get('systemlogics.modula')
+
+        products = super(Product, cls).create(vlist)
+
+        systemlogics_products = []
+        for product in products:
+            if product.code and not product.codes:
+                systemlogics_products.append(product)
+        if systemlogics_products:
+            SystemLogicsModula.imp_articoli(systemlogics_products)
+        return products
+
+    @classmethod
+    def write(cls, *args):
+        SystemLogicsModula = Pool().get('systemlogics.modula')
+
+        super(Product, cls).write(*args)
+        print "product write"
+        systemlogics_products = []
+        actions = iter(args)
+        for products, values in zip(actions, actions):
+            if 'code' in values and not values.get('codes'):
+                systemlogics_products = products
+
+        if systemlogics_products:
+            SystemLogicsModula.imp_articoli(systemlogics_products)
 
 
 class ProductCode:
     __name__ = 'product.code'
 
     @classmethod
-    def validate(cls, codes):
-        super(ProductCode, cls).validate(codes)
-        for code in codes:
-            code.systemlogics_modula()
+    def create(cls, vlist):
+        pool = Pool()
+        SystemLogicsModula = pool.get('systemlogics.modula')
+        Product = pool.get('product.product')
 
-    def systemlogics_modula(self):
-        SystemLogicsModula = Pool().get('systemlogics.modula')
+        codes = super(ProductCode, cls).create(vlist)
 
-        if self.barcode in _TYPE_BARCODES and self.number:
-            SystemLogicsModula.imp_articoli([self.product])
+        vlist = [v.copy() for v in vlist]
+        systemlogics_products = set()
+        for values in vlist:
+            systemlogics_products.add(values.get('product'))
+        if systemlogics_products:
+            products = Product.browse(list(systemlogics_products))
+            SystemLogicsModula.imp_articoli(products)
+        return codes
+
+    @classmethod
+    def write(cls, *args):
+        pool = Pool()
+        SystemLogicsModula = pool.get('systemlogics.modula')
+        Product = pool.get('product.product')
+
+        super(ProductCode, cls).write(*args)
+
+        codes = sum(args[::2], [])
+        systemlogics_products = set(c.product for c in codes)
+        if systemlogics_products:
+            products = Product.browse(list(systemlogics_products))
+            SystemLogicsModula.imp_articoli(products)
