e306ad019fcc4377cdcec9add5398aeea54c2750
resteve <resteve@zikzakmedia.com>
2015-12-02 09:30:55 +0100

    FIX loading Tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 #This file is part systemlogics_modula module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-from .test_systemlogics import suite
+from .test_systemlogics_modula import suite
 
 __all__ = ['suite']
